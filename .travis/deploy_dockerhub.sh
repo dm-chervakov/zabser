b91d8ce6a795c6bf0f2d5da42072469dcdd919cb
dm-yehorov <dm.yehorov@gmail.com>
2019-11-09 19:19:31 +0200

    Deploy with bash script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+docker tag zabbix-server $DOCKER_USER/zabbix-server
+docker push $DOCKER_USER/zabbix-server
+
