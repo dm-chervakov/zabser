4f8df62393d5fe237e3f7e2b6601af65341ee30d
dm-yehorov <dm.yehorov@gmail.com>
2019-11-10 16:31:35 +0200

    Varibles name server

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker tag zabser $DOCKER_USER/zabser
-docker push $DOCKER_USER/zabser
+docker tag $DOCKER_NAME_IMAGE $DOCKER_USER/$DOCKER_NAME_IMAGE
+docker push $DOCKER_USER/$DOCKER_NAME_IMAGE
 
